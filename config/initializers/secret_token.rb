5373ea9f24354ed558a9e1c0a504964dd9432084
get --version <get -version>
2013-02-13 20:19:38 -0500

    subir proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusicaUPC::Application.config.secret_token = '7da40216c0c97a2a743716db954417cb24daf1d04b5c27052c0a882580c7286832e7278afda0fa6dd744a0b27ec5f95a6db8cb97425b2ed169fc5735e1683955'
